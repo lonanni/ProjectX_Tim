2fe37bbabba9f9d7bb6d92fb335ce1a58eb8506a
Joseph <jl02392@surrey.ac.uk>
2024-07-11 15:39:45 +0100

    CAN Bus demo scripts using the MCP

---
+++ mcp_mcp_can/mcp_mcp_can_write.ino
@@ -0,0 +1,54 @@
+#include <CAN.h>
+#include <CANController.h>
+#include <ESP32SJA1000.h>
+#include <MCP2515.h>
+
+// Copyright (c) Sandeep Mistry. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <CAN.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("CAN Sender");
+
+  // start the CAN bus at 500 kbps
+  if (!CAN.begin(500E3)) {
+    Serial.println("Starting CAN failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // send packet: id is 11 bits, packet can contain up to 8 bytes of data
+  Serial.print("Sending packet ... ");
+
+  CAN.beginPacket(0x12);
+  CAN.write('h');
+  CAN.write('e');
+  CAN.write('l');
+  CAN.write('l');
+  CAN.write('o');
+  CAN.endPacket();
+
+  Serial.println("done");
+
+  delay(1000);
+
+  // send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
+  Serial.print("Sending extended packet ... ");
+
+  CAN.beginExtendedPacket(0xabcdef);
+  CAN.write('w');
+  CAN.write('o');
+  CAN.write('r');
+  CAN.write('l');
+  CAN.write('d');
+  CAN.endPacket();
+
+  Serial.println("done");
+
+  delay(1000);
+}
